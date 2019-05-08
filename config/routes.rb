e8c232b190eaff95aee70a47bd56e8c8645a9e10
github username <chao.alexander.123@gmail.com>
2019-05-08 13:16:42 -0400

    add cards as a nested resource within decks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :decks
+      resources :decks do
+        resources :cards
+      end
     end
   end
 end
