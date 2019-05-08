929579b101a25176be372cce7b354ae2d7b333f4
github username <chao.alexander.123@gmail.com>
2019-05-07 23:22:05 -0400

    define generate_cards method to create all the cards within a deck when a deck object is created

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,17 @@
 class Deck < ApplicationRecord
   belongs_to :user
   has_many :cards
+
+  after_create :generate_cards
+
+  def generate_cards
+    suits = ['Club', 'Diamond', 'Heart', 'Spade']
+    ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+    suits.each do |suit|
+      ranks.each do |rank|
+        Card.create(deck: self, rank: rank, suit: suit)
+      end
+    end
+  end
 end
