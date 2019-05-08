31b2617c43f61f95c7f56cf6a0a747cc4eacbf13
github username <chao.alexander.123@gmail.com>
2019-05-08 12:56:31 -0400

    modify cors.rb to accept requests from all origins

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -9,11 +9,23 @@ module Api
         render json: decks
       end
 
+      def create
+        deck = Deck.new()
+        if deck.save
+          render json: deck, status: :accepted
+        else
+          render json: { errors: deck.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+
       def show
         deck = Deck.find_by(id: params[:id])
         render json: deck
       end
 
+      def draw
+
+      end
     end
   end
 end
